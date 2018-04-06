be2f15c5a93c9c821eb6e7a4d27e706e75e21cbe
tandy <tandy0343@gmail.com>
2018-04-06 14:58:01 +0800

    docker 尝试

--- spring-boot-banner/src/main/java/com/neo/banner/BannerApplication.java
+++ spring-boot-banner/src/main/java/com/neo/banner/BannerApplication.java
@@ -1,5 +1,6 @@
 package com.neo.banner;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BannerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BannerApplication.class, args);
+		SpringApplication app = new SpringApplication(BannerApplication.class);
+//		SpringApplication.run(BannerApplication.class, args);
+//		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }
